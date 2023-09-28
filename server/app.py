c861435b7882f14c9b0e691f5ba6543b08b32324
Mohamed Hussein <mohamedmhussein@protonmail.com>
2023-09-28 19:54:47 -0400

    done

--- server/app.py
+++ server/app.py
@@ -7,20 +7,127 @@ from sqlalchemy.exc import IntegrityError
 from config import app, db, api
 from models import User, Recipe
 
+
 class Signup(Resource):
-    pass
+    def post(self):
+
+        try:
+            request.get_json()['username']
+        except KeyError:    
+            return {"Message":"Unprocessable Entity" }, 422
+        
+        try:
+            request.get_json()['image_url']
+        except KeyError: 
+                user = User(
+                username=request.get_json()['username'],
+            )
+        else:
+            user = User(
+            username=request.get_json()['username'],
+            image_url=request.get_json()['image_url'],
+            bio=request.get_json()['bio']
+           )
+        user.password_hash = request.get_json()['password']
+        db.session.add(user)
+        db.session.commit()
+        session['user_id'] = user.id
+        return (
+            {
+                "id" : user.id,
+                "username" : user.username,
+                "image_url" : user.image_url,
+                "bio" : user.bio
+            }
+        ), 201
 
 class CheckSession(Resource):
-    pass
+    def get(self):
+        user=User.query.filter(User.id == session.get('user_id')).first()
+
+        if user:
+            return (
+            {
+                "id" : user.id,
+                "username" : user.username,
+                "image_url" : user.image_url,
+                "bio" : user.bio
+            }
+            ),200
+        else:
+            return {"Message": "Unauthorized"}, 401
 
 class Login(Resource):
-    pass
+    def post(self):
+
+        username = request.get_json()['username']
+        user = User.query.filter(User.username == username).first()
+        
+        if user:
+            #get password
+            password = request.get_json()['password']
+
+            # if the password authenticates then set the session
+            # with the user's id
+            if user.authenticate(password):
+                session['user_id'] = user.id
+                return (
+                {
+                    "id" : user.id,
+                    "username" : user.username,
+                    "image_url" : user.image_url,
+                    "bio" : user.bio
+                } 
+                ),201
+        return {'error': 'Invalid username or password'}, 401   
 
 class Logout(Resource):
-    pass
+    def delete(self):
+
+        if session.get("user_id"):
+            session['user_id'] = None
+            return {}, 204
+        return {"message": "unauthorized"}, 401
 
 class RecipeIndex(Resource):
-    pass
+    def get(self):
+
+        user=User.query.filter(User.id == session.get('user_id')).first()
+        
+        if user:
+            recipes =[recipe.to_dict() for recipe in Recipe.query.all()]
+            return (recipes),200
+        else:
+            return {"message":"unauthorized"}, 401
+
+    def post(self):
+        user=User.query.filter(User.id == session.get('user_id')).first()
+        
+        if user:
+            json = request.get_json()
+
+            try:
+                recipe = Recipe(
+                    title = json['title'],
+                    instructions = json['instructions'],
+                    minutes_to_complete = json['minutes_to_complete'],
+                    user_id = session['user_id']
+                )
+                db.session.add(recipe)
+                db.session.commit()
+            except IntegrityError:
+                  return {"message":"Unprocessable Entity"}, 422  
+
+            return (
+                {
+                    "title" : recipe.title,
+                    "instructions" : recipe.instructions,
+                    "minutes_to_complete" : recipe.minutes_to_complete,
+                    "user_id" : recipe.user_id       
+                }
+            ),201
+        else:
+            return {"message":"unauthorized"}, 401
 
 api.add_resource(Signup, '/signup', endpoint='signup')
 api.add_resource(CheckSession, '/check_session', endpoint='check_session')
--- server/models.py
+++ server/models.py
@@ -4,11 +4,54 @@ from sqlalchemy_serializer import SerializerMixin
 from config import db, bcrypt
 
 class User(db.Model, SerializerMixin):
+
     __tablename__ = 'users'
 
-    pass
+    serialize_rules = ('-recipies.user',)
+       
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    _password_hash = db.Column(db.String)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', backref='user')
+
+    @hybrid_property
+    def password_hash(self):
+        if self._password_hash:
+            raise AttributeError('Password hashes may not be viewed.')
+            
+    @password_hash.setter
+    def password_hash(self, password):
+        
+        password_hash = bcrypt.generate_password_hash( password.encode('utf-8'))
+        
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
+    def __repr__(self):
+        return f' User: {self.username} ID: {self.id} Image: {self.image_url} BIO: {self.bio}'
+    
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
+
+    __table_args__ = (
+        db.CheckConstraint('length(instructions) >= 50'),
+    )
+
+    serialize_rules = ('-user.recipes',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String)
+    minutes_to_complete = db.Column(db.Integer)
+
+    user_id = db.Column(db.Integer, db.ForeignKey ('users.id'))
     
-    pass
+    def __repr__(self):
+        return f' ID: {self.id} Title: {self.title} Instructions: {self.instructions}'
